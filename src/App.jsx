082188092c534cc017cb4829e63607912e07c8fe
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-06-06 03:14:47 +0200

    installed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Suspense } from 'react'
+import Layout from './components/Layout'
 
 function App() {
 
   return (
-    <>
-  HELLO
-    </>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+
+      </Routes>
+    </Suspense>
   )
 }
 
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import { Main } from "./Layout.module"
+
+const Layout = () => {
+    return (
+        <Main>
+            Hello Layout
+        </Main>
+    )
+};
+
+export default Layout;
